8675d209067d71369b38bb7e50d0509564857b73
Light555 <2277495183@qq.com>
2024-03-30 19:08:38 +0800

    打印message 耗时

--- source/src/main/java/com/ligh/block/source/handler/LooperMonitor.java
+++ source/src/main/java/com/ligh/block/source/handler/LooperMonitor.java
@@ -5,6 +5,32 @@ import android.util.Printer;
 
 import javax.inject.Inject;
 
+/**
+ * 原理基于 Looper.loop()
+ * public static void loop() {
+ *     ...
+ *
+ *     for (;;) {
+ *         ...
+ *
+ *         // This must be in a local variable, in case a UI event sets the logger
+ *         Printer logging = me.mLogging;
+ *         if (logging != null) {
+ *             logging.println(">>>>> Dispatching to " + msg.target + " " +
+ *                     msg.callback + ": " + msg.what);
+ *         }
+ *
+ *         msg.target.dispatchMessage(msg);
+ *
+ *         if (logging != null) {
+ *             logging.println("<<<<< Finished to " + msg.target + " " + msg.callback);
+ *         }
+ *
+ *         ...
+ *     }
+ * }
+ */
+
 
 public class LooperMonitor implements Printer {
 
