7126be664facbebf6789316d146896fdcb999328
Ligh555 <lghkele@gmail.com>
2025-01-05 17:11:44 +0800

    build test

--- app/src/main/java/com/ligh/blog/activity/TestActivity.kt
+++ app/src/main/java/com/ligh/blog/activity/TestActivity.kt
@@ -1,11 +1,47 @@
 package com.ligh.blog.activity
 
+import android.widget.Toast
 import com.ligh.base.activity.binding
 import com.ligh.blog.databinding.ActivityTestBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.suspendCoroutine
 
 class TestActivity : BaseActivity() {
     override val viewBinding: ActivityTestBinding by  binding()
     override fun initViewBinding() {
+
+        viewBinding.run {
+            btLock.setOnClickListener {
+                GlobalScope.launch {
+                    withContext(Dispatchers.IO){
+                        suspendCoroutine {
+
+                        }
+                    }
+                }
+            }
+
+            btNormal.setOnClickListener {
+                GlobalScope.launch {
+                    withContext(Dispatchers.IO){
+                       withContext(Dispatchers.Main){
+                           Toast.makeText(this@TestActivity,"ceshishis",Toast.LENGTH_SHORT).show()
+                       }
+                    }
+                }
+            }
+
+            btOther.setOnClickListener {
+                GlobalScope.launch {
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@TestActivity,"ceshishis",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
     }
 }
