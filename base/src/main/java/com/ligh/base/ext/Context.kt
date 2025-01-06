7c990b574bbc4345de1f7a1e3f78abdfa172f9d4
Ligh555 <lghkele@gmail.com>
2025-01-07 00:05:55 +0800

    添加 getBitMap

--- base/src/main/java/com/ligh/base/ext/Context.kt
+++ base/src/main/java/com/ligh/base/ext/Context.kt
@@ -1,26 +1,20 @@
-package com.ligh.base.utils
+package com.ligh.base.ext
 
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.TypedValue
 import androidx.annotation.DrawableRes
 
-fun dp2px(value :Float):Float{
-    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,value,Resources.getSystem().displayMetrics)
-}
-
-fun getBitMap(context: Context,@DrawableRes id :Int,width :Int):Bitmap{
+fun Context.getBitMap( @DrawableRes id :Int, width :Int): Bitmap{
     val options = BitmapFactory.Options()
     //第一次读取只读边界获取宽高
     options.inJustDecodeBounds = true
-    BitmapFactory.decodeResource(context.resources,id,options)
+    BitmapFactory.decodeResource(resources,id,options)
     // 第二次读取目标尺寸的图片
     options.inJustDecodeBounds =false
     //原始宽度
     options.inDensity = options.outWidth
     //目标宽度
     options.outWidth = width
-    return  BitmapFactory.decodeResource(context.resources,id,options)
+    return  BitmapFactory.decodeResource(resources,id,options)
 }
