624f11f7dd8475afa5fd2a831b4cbf7b635cb62a
Ligh555 <lghkele@gmail.com>
2025-02-23 16:03:43 +0800

    ScaleImageView  添加滑动和惯性滑动

--- android/src/main/java/com/ligh/block/source/view/ScaleImageView.kt
+++ android/src/main/java/com/ligh/block/source/view/ScaleImageView.kt
@@ -6,17 +6,16 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
-import android.widget.ImageView
-import androidx.appcompat.content.res.AppCompatResources
+import android.widget.OverScroller
 import androidx.appcompat.widget.AppCompatImageView
 import com.ligh.base.ext.dp2px
 import com.ligh.base.utils.getBitMap
 import com.ligh.block.source.R
+import kotlin.math.max
 
 /**
  * 1、 如何设计图片尺寸
@@ -33,6 +32,12 @@ class ScaleImageView @JvmOverloads constructor(
     private val bigScale = 5f
     private val smallScale = 1f
 
+    private var offsetX = 0f
+    private var offsetY = 0f
+
+    private var translateX = 0f
+    private var translateY = 0f
+
     var scaleFactory = smallScale
     private val bitmap: Bitmap by lazy {
         getBitMap(context, R.drawable.test_screen, IMAGE_SIZE)
@@ -44,10 +49,12 @@ class ScaleImageView @JvmOverloads constructor(
         this.strokeWidth = 1.5f.dp2px()
     }
 
-    val animator = ObjectAnimator.ofFloat(smallScale,bigScale).apply {
+    private val overScroller = OverScroller(context)
+
+    private val animator = ObjectAnimator.ofFloat(smallScale, bigScale).apply {
         duration = 100
         addUpdateListener {
-            scaleFactory = 1 +  (it.animatedFraction * (bigScale - smallScale) )
+            scaleFactory = 1 + (it.animatedFraction * (bigScale - smallScale))
             Log.i("ScaleImageView", "scaleFactory: ${it.animatedFraction} $scaleFactory ")
             invalidate()
         }
@@ -57,23 +64,85 @@ class ScaleImageView @JvmOverloads constructor(
     private val gestureDetector: GestureDetector = GestureDetector(context, object :
         GestureDetector.SimpleOnGestureListener() {
         override fun onDoubleTap(e: MotionEvent): Boolean {
-            if (animator.isRunning){
+            if (animator.isRunning) {
                 animator.cancel()
             }
             if (scaleFactory == bigScale) {
                 animator.reverse()
+                translateX = 0f
+                translateY = 0f
             } else {
                 animator.start()
             }
             return true // 表示处理事件
         }
+
+        override fun onScroll(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            if (scaleFactory != smallScale) {
+                translateX -= distanceX
+                val maxX = (scaleFactory * bitmap.width - width) / 2
+                val maxY = (scaleFactory * bitmap.height - height) / 2
+                translateX = translateX.coerceAtLeast(-maxX)
+                translateX = translateX.coerceAtMost(maxX)
+                translateY -= distanceY
+                translateY = translateY.coerceAtLeast(-maxY)
+                translateY = translateY.coerceAtMost(maxY)
+                invalidate()
+            }
+            return true
+        }
+
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            val maxX = ((scaleFactory * bitmap.width - width) / 2).toInt()
+            val maxY = ((scaleFactory * bitmap.height - height) / 2).toInt()
+            overScroller.fling(
+                translateX.toInt(),
+                translateY.toInt(),
+                velocityX.toInt(),
+                velocityY.toInt(),
+                -maxX,
+                maxX,
+                -maxY,
+                maxY
+            )
+            refreshTranslate()
+            return true
+        }
     })
+
+    private fun refreshTranslate() {
+        postOnAnimation {
+            if (overScroller.computeScrollOffset()) {
+                translateX = overScroller.currX.toFloat()
+                translateY = overScroller.currY.toFloat()
+                invalidate()
+                refreshTranslate()
+            }
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        offsetX = (w - bitmap.width) / 2f
+        offsetY = (h - bitmap.height) / 2f
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.translate(translateX, translateY)
         canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), boardPaint)
         canvas.scale(scaleFactory, scaleFactory, width / 2f, height / 2f) //注意要设置居中点,以居中点为中心开始放大
-        canvas.drawBitmap(bitmap, (width - bitmap.width) / 2f, (height - bitmap.height) / 2f, paint)
+        canvas.drawBitmap(bitmap, offsetX, offsetY, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
